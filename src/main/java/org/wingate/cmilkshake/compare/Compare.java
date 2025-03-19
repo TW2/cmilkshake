22dc2d4cae22ad64681f6cc7409dade5f7baf3ba
RedDog,csa <util2@msn.com>
2025-03-19 18:52:49 +0100

    Compare and sort

--- src/main/java/org/wingate/cmilkshake/compare/Compare.java
+++ src/main/java/org/wingate/cmilkshake/compare/Compare.java
@@ -6,8 +6,6 @@ import org.wingate.cmilkshake.ui.ViewState;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Compare {
 
