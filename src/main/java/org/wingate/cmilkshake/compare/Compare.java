d8d28e39810749415064a718aa31acd196ef5d1e
RedDog,csa <util2@msn.com>
2025-03-19 18:51:46 +0100

    Compare and sort

--- src/main/java/org/wingate/cmilkshake/compare/Compare.java
+++ src/main/java/org/wingate/cmilkshake/compare/Compare.java
@@ -6,6 +6,8 @@ import org.wingate.cmilkshake.ui.ViewState;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Compare {
 
@@ -14,39 +16,80 @@ public class Compare {
     }
 
     public static List<View> getSentenceViews(List<Event> a, List<Event> b){
-        final List<View> c = new ArrayList<>();
-
-        boolean exists;
+        final List<View> deletedViews = new ArrayList<>();
+        final List<View> addedViews = new ArrayList<>();
+        final List<View> sameViews = new ArrayList<>();
 
         for(Event ea : a){
-            exists = false;
+            String sentence_A = ea.getText();
+            boolean found = false;
             for(Event eb : b){
-                if(ea.getText().equals(eb.getText())){
-                    exists = true;
-                    c.add(new View(ea, ViewState.SameContent));
+                String sentence_B = eb.getText();
+                if(sentence_A.equals(sentence_B)){
+                    found = true;
                     break;
                 }
             }
-            // If exists == false then A has a old sentence
-            if(!exists){
-                c.add(new View(ea, ViewState.HasBeenDeleted));
+            if(!found){
+                deletedViews.add(new View(ea, ViewState.HasBeenDeleted));
+            }else{
+                sameViews.add(new View(ea, ViewState.SameContent));
             }
         }
 
         for(Event eb : b){
-            exists = false;
-            for(Event ea : a){
-                if(ea.getText().equals(eb.getText())){
-                    exists = true;
+            String sentence_B = eb.getText();
+            boolean found = false;
+            for(View deleted : deletedViews){
+                if(deleted.getEvent().getText().equals(sentence_B)){
+                    found = true;
                     break;
                 }
             }
-            // If exists == false then B has a new sentence
-            if(!exists){
-                c.add(new View(eb, ViewState.NewContent));
+            for(View same : sameViews){
+                if(!found && same.getEvent().getText().equals(sentence_B)){
+                    found = true;
+                    break;
+                }
+            }
+            if(!found){
+                addedViews.add(new View(eb, ViewState.NewContent));
             }
         }
 
+        final List<View> c = new ArrayList<>();
+        c.addAll(sameViews);
+        c.addAll(deletedViews);
+        c.addAll(addedViews);
+
+        c.sort((v1, v2) -> {
+            long startA = v1.getEvent().getMicrosStart();
+            long startB = v2.getEvent().getMicrosStart();
+            return Long.compare(startA, startB);
+        });
+
+        c.sort((v1, v2) -> {
+            String startsWithA = strip(v1.getEvent().getText());
+            String startsWithB = strip(v2.getEvent().getText());
+            String sA = startsWithA.substring(0, Math.min(25, Math.max(0, startsWithA.length()-1)));
+            String sB = startsWithB.substring(0, Math.min(25, Math.max(0, startsWithB.length()-1)));
+            return sA.compareTo(sB);
+        });
+
         return c;
+    }
+
+    private static String strip(String text){
+        String str;
+        if(text.contains("{")){
+            try{
+                str = text.replaceAll("\\{[^\\}]+\\}", "");
+            }catch(Exception e){
+                str = text;
+            }
+        }else{
+            str = text;
+        }
+        return str;
     }
 }
